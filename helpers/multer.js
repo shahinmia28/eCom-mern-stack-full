0454b0796a19277d7b59ff5f7fa1e75816cdc1fc
Shahin Mia <shahin01777296933@gmail.com>
2024-01-24 20:04:30 +0600

    Update multer.js

--- helpers/multer.js
+++ helpers/multer.js
@@ -4,7 +4,7 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './helpers/');
   },
-
+ 
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
   },
